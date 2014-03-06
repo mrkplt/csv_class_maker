c6751fc5cea4a7b9e4990a7231685e95edaba33b
Mark Platt <mplatt@weblinc.com>
2014-03-06 13:47:15 -0500

    Somethin ain't right

--- spec/csv_reader_spec.rb
+++ spec/csv_reader_spec.rb
@@ -14,7 +14,10 @@ end
 describe Object do
   before(:all){
     @person1 = People.new(first: 'Mark', last: 'Platt', nickname: 'JCool')
+    @person2 = People.new(first: 'Longinus', last: 'Smith', nickname: 'Pebbles')
+    @person3 = People.new(first: 'Charlie', last: 'Mansfield', nickname: 'Sammykins')
   }
+
   context 'class methods' do
     it "responds to .first" do
       (People).should respond_to(:first)
@@ -26,13 +29,13 @@ describe Object do
       (People).should respond_to(:last)
     end
     it "responds to .last" do
-      (People).should respond_to(:last)
+      expect(People.last).to eq @person3
     end
     it "responds to .find" do
       (People).should respond_to(:find)
     end
     it "responds to .find" do
-      (People).should respond_to(:find)
+      expect(People.find(3)).to eq @person2
     end
     it "responds to .find_by" do
       (People).should respond_to(:find_by)
